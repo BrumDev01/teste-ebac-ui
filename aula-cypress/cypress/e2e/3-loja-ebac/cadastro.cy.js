dbb6f1b46d6c3c8d0120fd7cf193a5d412c7715d
João <joaomarcosbrum@hotmai.com>
2024-06-26 14:46:50 -0300

    alterações com comandos customizados

--- aula-cypress/cypress/e2e/3-loja-ebac/cadastro.cy.js
+++ aula-cypress/cypress/e2e/3-loja-ebac/cadastro.cy.js
@@ -4,7 +4,7 @@ import { faker } from '@faker-js/faker';
 describe('Funcionalidade: Cadastro', () => {
    
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('/minha-conta')
         
         
     });
@@ -39,9 +39,15 @@ it('Deve completar o cadastro com sucesso -  Usando variáveis', () => {
     cy.get('.woocommerce-Button').click()	
     cy.get('.woocommerce-message').should('contain','Detalhes da conta modificados com sucesso.')
 
-        });
+});
+
+it.only('Deve completar o cadastro com sucesso -  Usando comando customizado', () => {
+    cy.preCadastro(faker.internet.email(),'teste.teste',faker.person.firstName(), faker.person.lastName() )
+
+
 
 });
 
 
 
+})
