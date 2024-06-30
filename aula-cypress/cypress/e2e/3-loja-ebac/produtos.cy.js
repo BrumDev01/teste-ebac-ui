2661b0a6149538d8371991f8b8315f69cb5d9e39
João <joaomarcosbrum@hotmai.com>
2024-06-30 19:14:35 -0300

    Revisao

--- aula-cypress/cypress/e2e/3-loja-ebac/produtos.cy.js
+++ aula-cypress/cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,23 +1,42 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objetcs/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+      produtosPage.visitarUrl()
 
 
     });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block')
-            //.first()
-            //.last()
-            //.eq(2)
-            .contains('Aero Daily Fitness Tee')
-            .click()
-
-            cy.get('#tab-title-description > a').should('contain','Descrição')
-            
+      produtosPage.buscarprodutoLista('Aero Daily Fitness Tee')
+      cy.get('#tab-title-description > a').should('contain', 'Descrição')
         
     });
-});
+
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Aether Gym Pant'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain',produto)
+    });
+
+    it('Deve visitar a página do produto', () => {
+      produtosPage.visitarProduto('Aether Gym Pant')
+      cy.get('.product_title').should('contain','Aether Gym Pant')
+        
+    });
+
+    it.only('Deve adicionar produto ao carrinho', () => {
+      const qtd = 7
+      produtosPage.buscarProduto('Ariel Roll Sleeve Sweatshirt')
+      produtosPage.addProdutoCarrinho('L', 'Red', qtd)
+
+      cy.get('.woocomerce-message').should('contain', qtd + 'x "Ariel Roll Sleeve Sweatshirt" foram adicionados no seu carrinho.')
+
+    });
+
+  });
+
+  
+     
